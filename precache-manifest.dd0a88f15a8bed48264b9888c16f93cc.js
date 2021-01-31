60ce6587351b5471dc3f5c59930d661fe4577023
José Ignacio Trava García <travag97@gmail.com>
2021-01-30 21:07:30 -0600

    Updates

---
+++ precache-manifest.dd0a88f15a8bed48264b9888c16f93cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264fc26cb758ab991aca50f5bdc88d73",
+    "url": "/react-code-challenge/index.html"
+  },
+  {
+    "revision": "95ecba7d4fc903b01f53",
+    "url": "/react-code-challenge/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "99090e1b30e19a4c2973",
+    "url": "/react-code-challenge/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "95ecba7d4fc903b01f53",
+    "url": "/react-code-challenge/static/js/2.5fccbfc6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-code-challenge/static/js/2.5fccbfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99090e1b30e19a4c2973",
+    "url": "/react-code-challenge/static/js/main.8b1aa648.chunk.js"
+  },
+  {
+    "revision": "996a1e3f144781fd3e60",
+    "url": "/react-code-challenge/static/js/runtime-main.76160a2a.js"
+  }
+]);
